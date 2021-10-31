5ded4757868ba93d9c616c28a42bcadac823532d
marescanog <marescanog@gmail.com>
2021-10-31 19:58:31 +0800

    adding quick html

--- index.php
+++ index.php
@@ -7,4 +7,16 @@ $dotenv->load();
 // $db_host = $_SERVER['DB_HOST'];
 $db_host = getenv('DB_HOST');
 
-var_dump($db_host);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOMEHERO API</title>
+</head>
+<body>
+    <p>This server is connected to: <?php echo isset($db_host) ? $db_host : "No Connection established"; ?></p>
+</body>
+</html>
